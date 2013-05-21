587e5e48d398dbac8c4ec0c71112be06f7400b30
Lucas Yuji Suguinoshita Aciole <lucas.aciole@gmail.com>
2013-05-21 16:24:53 -0300

    Tornando usuário confirmável, recuperável e trackable

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,8 @@
 class User < ActiveRecord::Base
-  devise :database_authenticatable, :registerable, :validatable
+  devise :database_authenticatable, :registerable, :validatable, :recoverable, :trackable, :confirmable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :admin
+  attr_accessible :name, :email, :password, :password_confirmation, :remember_me, :confirmed_at, :admin
 
   has_many :sales
 end
